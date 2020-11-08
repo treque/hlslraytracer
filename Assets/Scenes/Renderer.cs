f436b03c0e9c5053b04ce6b2e1d601186cf3c2af
Huyen Trang Dinh <tran9dinh@gmail.com>
2020-11-07 19:23:44 -0500

    sphere

--- Assets/Scenes/Renderer.cs
+++ Assets/Scenes/Renderer.cs
@@ -16,12 +16,63 @@ public class Renderer : MonoBehaviour
     uint currentSample = 0;
     Material addMaterial;
 
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    };
+
     void Awake()
     {
         camera = GetComponent<Camera>();
     }
 
-    private void Update()
+    void OnEnable()
+    {
+        currentSample = 0;
+        SetUpSpheres();
+    }
+
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null) _sphereBuffer.Release();
+    }
+
+    void SetUpSpheres()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+        for (int i =0; spheres.Count < SpheresMax; ++i)
+        {
+            Sphere sphere = new Sphere();
+            sphere.radius = SphereRadii.x + Random.value * (SphereRadii.y - SphereRadii.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            bool skip = false;
+            foreach (Sphere otherSphere in spheres)
+            {
+                float minDist = sphere.radius + otherSphere.radius;
+                if (Vector3.SqrMagnitude(sphere.position - otherSphere.position) < minDist * minDist) skip = true;
+            }
+
+            if (!skip)
+            {
+                Color color = Random.ColorHSV();
+                bool metallic = Random.value < 0.5f;
+                sphere.albedo = metallic ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+                sphere.specular = metallic ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+                spheres.Add(sphere);
+            }
+        }
+
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+        _sphereBuffer.SetData(spheres);
+
+    }
+
+    void Update()
     {
         // si la camera bouge pas on va pas resample..
         if (transform.hasChanged)
@@ -46,6 +97,7 @@ public class Renderer : MonoBehaviour
         RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
         RayTracingShader.SetInt("_RayBounces", RayBounces);
         RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
     }
 
     // called whenevr camera is finished rendering
